8e614d45fc8b029e02d4d26cfc54e8f6bdcbe217
swaperX <mesut.nedzhib@gmail.com>
2021-09-24 17:44:22 +0300

    commit

--- client/src/views/MovieDetailsPage/MovieDetailsPage.js
+++ client/src/views/MovieDetailsPage/MovieDetailsPage.js
@@ -1,15 +1,39 @@
-import React from "react";
+import React, { useState } from "react";
 import "./MovieDetailsPage.scss";
 import Movie from "../../components/Movie/Movie";
+import StarRatings from "react-star-ratings";
 
 function MovieDetailsPage() {
+  const [rating, setRating] = useState();
+  console.log(rating);
+
+  const changeRating = (newRating) => {
+    setRating(newRating);
+  };
+
   return (
     <div className="movieDetailsPage">
       <div className="movie-details-section">
         <Movie />
       </div>
-          <div className="your-review-section">
-              
+      <div className="your-review-section">
+        <h2>Your Review</h2>
+        <StarRatings
+          rating={rating}
+          starRatedColor="yellow"
+          starHoverColor="yellow"
+          changeRating={changeRating}
+          numberOfStars={5}
+          name="rating"
+        />
+      </div>
+      <div className="form-group">
+        <textarea
+          style={{ resize: "none" }}
+          className="form-control w-50"
+          rows="5"
+          placeholder="Your private notes and comments about the movie..."
+        ></textarea>
       </div>
     </div>
   );
